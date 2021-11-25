0a47336216eb15229b541b7977ffc7a323f48199
Edmond Dantes <61810409+nameOfUser0236@users.noreply.github.com>
2021-11-25 08:01:15 -0600

    add test attributes

---
+++ addons/test/Attributes.cs
@@ -0,0 +1,34 @@
+namespace GodotTest
+{
+	using System;
+	using Godot;
+
+
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public class TestGroupAttribute : Attribute
+	{
+		public readonly string _title;
+		public TestGroupAttribute(string title){_title = title;}
+	}
+
+
+	[AttributeUsage(AttributeTargets.Method)]
+	public class TestAttribute : Attribute
+	{
+		public readonly string Title;
+		public readonly object[]? Args;
+		public TestAttribute(string title, params object[]? args)
+		{
+			Title = title;
+			Args = args;
+		}
+	}
+
+
+	[AttributeUsage(AttributeTargets.Method)]
+	public class SetupAttribute : Attribute{}
+
+
+	[AttributeUsage(AttributeTargets.Method)]
+	public class TearDownAttribute : Attribute{}
+}
