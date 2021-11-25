633f09375cec5fa1fb9592ad169c52f4e48cec8d
Edmond Dantes <61810409+nameOfUser0236@users.noreply.github.com>
2021-11-25 07:31:54 -0600

    add basic gui

--- addons/test/test.gd
+++ addons/test/test.gd
@@ -1,8 +1,14 @@
 tool
 extends EditorPlugin
 
+var dock;
+
 func _enter_tree():
-	print("test plugin loaded");
+	#print("test plugin loaded");
+	dock = preload("res://addons/test/scenes/test_gui.tscn").instance();
+	add_control_to_bottom_panel(dock, "Tests");
+
 
 func _exit_tree():
-	pass
+	remove_control_from_bottom_panel(dock);
+	dock.queue_free();
