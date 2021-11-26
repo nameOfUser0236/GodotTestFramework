6095cd7b649976508dab07bb9e9ea4ec8326148d
Edmond Dantes <61810409+nameOfUser0236@users.noreply.github.com>
2021-11-26 11:30:30 -0600

    move test helpers to seperate file

--- addons/test/Public.cs
+++ addons/test/Public.cs
@@ -37,43 +37,4 @@ namespace GodotTest
 		public TestFailedException(string message = "") : base(message)
 		{}
 	}
-
-	public static class TestHelpers
-	{
-		public static void IsTrue(bool boolean, string message = "IsTrue assert failed")
-		{
-			if(!boolean)
-			{
-				throw new TestFailedException(message);
-			}
-		}
-		public static void IsFalse(bool boolean, string message = "IsFalse assert failed")
-		{
-			if(!boolean)
-			{
-				throw new TestFailedException(message);
-			}
-		}
-		public static void IsEqual(object a, object b, string message = "IsEqual assert failed")
-		{
-			if(a != b)
-			{
-				throw new TestFailedException(message);
-			}
-		}
-		public static void IsEqual<T>(T a, T b, string message = "IsEqual<T> assert failed") where T : notnull, IEquatable<T>
-		{
-			if(!a.Equals(b))
-			{
-				throw new TestFailedException(message);
-			}
-		}
-		public static void IsNotEqual(object a, object b, string message = "IsNotEqual assert failed")
-		{
-			if(a == b)
-			{
-				throw new TestFailedException(message);
-			}
-		}
-	}
 }
